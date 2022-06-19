387b550a280ecbcafd513cbafb608361d495a488
ingalerohit1234 <107783201+ingalerohit1234@users.noreply.github.com>
2022-06-19 11:22:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+
+function play() {
+    var audio = document.getElementById('myaudio');
+     audio.play();
+     gif.style.opacity = 0.3;
+     
+}
+function pause(){
+    var audio = document.getElementById('myaudio');
+    audio.pause();
+    gif.style.opacity = 0.3 ;
+}
+
+function playa() {
+    var audio = document.getElementById('1.mp3');
+    audio.play();
+}
+function pausea(){
+    var audio = document.getElementById('1.mp3');
+    audio.pause();
+}
+
+function playb() {
+    var audio = document.getElementById('2.mp3');
+    audio.play();
+}
+function pauseb(){
+    var audio = document.getElementById('2.mp3');
+    audio.pause();
+}
+function playc() {
+    var audio = document.getElementById('3.mp3');
+    audio.play();
+}
+function pausec(){
+    var audio = document.getElementById('3.mp3');
+    audio.pause();
+}
+function playd() {
+    var audio = document.getElementById('4.mp3');
+    audio.play();
+}
+function paused(){
+    var audio = document.getElementById('4.mp3');
+    audio.pause();
+}
+function playe() {
+    var audio = document.getElementById('5.mp3');
+    audio.play();
+}
+function pausee(){
+    var audio = document.getElementById('5.mp3');
+    audio.pause();
+}
+function playf() {
+    var audio = document.getElementById('6.mp3');
+    audio.play();
+}
+function pausef(){
+    var audio = document.getElementById('6.mp3');
+    audio.pause();
+}
+function playg() {
+    var audio = document.getElementById('7.mp3');
+    audio.play();
+}
+function pauseg(){
+    var audio = document.getElementById('7.mp3');
+    audio.pause();
+}
+function playh() {
+    var audio = document.getElementById('8.mp3');
+    audio.play();
+}
+function pauseh(){
+    var audio = document.getElementById('8.mp3');
+    audio.pause();
+}
+function playi() {
+    var audio = document.getElementById('9.mp3');
+    audio.play();
+}
+function pausei(){
+    var audio = document.getElementById('9.mp3');
+    audio.pause();
+}
+function playj() {
+    var audio = document.getElementById('10.mp3');
+    audio.play();
+}
+function pausej(){
+    var audio = document.getElementById('10.mp3');
+    audio.pause();
+}
+// let icon = document.getElementById('icon');
+
+// icon.addEventListener('click', ()=>{
+//     if(Audio.paused || Audio.currentTime<=0){
+//         Audio.play();
+//     }
+// })
